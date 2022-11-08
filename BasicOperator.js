814f590f5cebdf445577462ebcc2ab4a9f508f6f
NguyenVanMinh <vanminhok12@gmail.com>
2022-11-08 09:47:52 +0700

    Minh

---
+++ BasicOperator.js
@@ -0,0 +1,15 @@
+var a= 20;
+let b= 23;
+let c= 23;
+d=c*b;
+h=a*b;
+document.write('a ='+a+'<br>');
+document.write('b = '+b+'<br>');
+document.write('c nhân b ='+d+'<br>');
+document.write('a nhân b ='+h);
+function showmessage() {
+    document.write('a = '+a +'<br>');
+    document.write('b = '+b);
+    document.write('c nhân b ='+d+'<br>');
+    document.write('a nhân b ='+h+'<br>');
+}
